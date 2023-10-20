036d62e519a23fa3689299618da76e3b061ca50f
John William <johnwilliams1756@gmail.com>
2023-10-20 13:40:30 +0300

    add json files to use dynamic data

---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+import { SuiteInfo } from "./SuiteInfo";
+import { Welcome } from "./Welcome";
+
+export const Main = () => {
+    return(
+        <main id="wrapper">
+            <Welcome/>
+            <SuiteInfo/>
+        </main>
+    );
+};
